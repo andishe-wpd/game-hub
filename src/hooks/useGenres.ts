20bcd7364c50bbadc936ec4bbd6f7e3dab3b250a
andishe <a.abbasian@pixaldo.com>
2023-06-05 17:59:42 +0330

    genres query implemented

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-
+import apiClient from "../services/api-client";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+// const useGenres = () => ({ data: genres, isLoading: false, error: null })
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGenres;
