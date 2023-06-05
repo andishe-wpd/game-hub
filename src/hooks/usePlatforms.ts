cc0e7feaf692d30d8692924f328817ea05a7c42a
andishe <a.abbasian@pixaldo.com>
2023-06-05 18:05:22 +0330

    implemented platform query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
 
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+
+// const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () => apiClient.get<FetchResponse<Platform>>("platforms").then((res) => res.data),
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
