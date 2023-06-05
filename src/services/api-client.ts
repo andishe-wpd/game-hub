eaad85beb62103aa807f09af36f3c164d2162470
andishe <a.abbasian@pixaldo.com>
2023-06-05 17:48:32 +0330

    project init

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: import.meta.env.VITE_GAME_API_KEY,
   },
 });
